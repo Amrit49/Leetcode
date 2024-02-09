94e89c4200ef4c0f9c26fd471e55084e88c1768f
Amritjot Kaur <amritjot.549@gmail.com>
2024-02-09 10:28:51 +0530

    containsDuplicate efficient

--- Leetcode/src/ARRAYS/containsDuplicate.java
+++ Leetcode/src/ARRAYS/containsDuplicate.java
@@ -1,17 +1,36 @@
 package ARRAYS;
 
+import java.util.*;
+//bruteforce
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+// 			for(int i= 0;i<nums.length;i++)
+// 			{
+// 				for(int j =i+1;j<nums.length;j++)
+// 				{
+// 						if(nums[i] == nums[j])
+// 						{
+// 							return true;
+// 						}
+// 				}
+// 			}
+// 			return false;
+//     }
+// }
+
+//efficient
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-			for(int i= 0;i<nums.length;i++)
-			{
-				for(int j =i+1;j<nums.length;j++)
-				{
-						if(nums[i] == nums[j])
-						{
-							return true;
-						}
-				}
-			}
-			return false;
+        Set<Integer> set = new HashSet<>();
+
+        for(int num : nums)
+        {
+            if(!set.add(num))
+            {
+                return true;
+            }
+        }
+		return false;
     }
 }
