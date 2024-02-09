5b36abe1ee00f32235a71d1f3159ae64ab540693
Amritjot Kaur <amritjot.549@gmail.com>
2024-02-09 10:15:33 +0530

    Pascal's Triangle

--- Leetcode/src/ARRAYS/generate.java
+++ Leetcode/src/ARRAYS/generate.java
@@ -1,5 +1,32 @@
 package ARRAYS;
 //Pascal tree
-public class generate {
-    
+import java.util.*;
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        List<Integer> row = new ArrayList<Integer>();
+        for(int i= 0; i<numRows;++i)
+        {
+            row = new ArrayList<Integer>();
+            for(int j =0;j<=i;++j)
+            {
+                if(j == 0|| j==i)
+                {
+                    row.add(1);
+                }
+                else
+                {
+                    List<Integer> prev = result.get(i - 1);
+                    int sum = prev.get(j-1) + prev.get(j);
+                    row.add(sum);
+                }
+                // prev =row;
+                
+            }
+            result.add(row);
+
+        }
+         return result;
+    }
 }
